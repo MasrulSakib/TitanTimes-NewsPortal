73db7bd0343eaf7a01901e3a676aeed97fa040ad
MasrulSakib <Masrul.Sakib69@gmail.com>
2023-08-19 03:17:17 +0600

    blog panal completed

--- JavaScript/portal.js
+++ JavaScript/portal.js
@@ -201,15 +201,51 @@ const dynamicModal = (element) => {
     const detailsTextHeader = document.getElementById('exampleModalLabel');
     detailsTextHeader.innerText = element.title;
     const portalInnerInfo = document.getElementById('newsInnerInfo');
-
     portalInnerInfo.innerHTML = `
         <p> Details: ${element.details}</p><br>
-        <p> Author: ${element.author.name}</p>
+        <p> Author: ${element.author.name === null || element.author.name === "" ? "No Data Found" : element.author.name}</p>
     
     `
     toggleLoader(false);
+}
 
-};
+
+const blogContent = () => {
+    const portalInnerHTML = document.getElementById('offcanvasWithBothOptions');
+    const title = document.getElementById('offcanvasWithBothOptionsLabel')
+    title.innerText = `Welcome to the Blog`;
+    const portalInnerInfo = document.getElementById('blog-container');
+    portalInnerInfo.innerHTML = `
+
+        <ul class = "list-group list-group-numbered">
+            <li> 
+                <h5>Q1: What is the difference between var, let, const?</h5>
+                <p>Ans: var is function-scoped and can be re-declared. let is block-scoped and can be reassigned.
+                        const is block-scoped and cannot be reassigned after declaration.</p>
+            </li>
+            <li> 
+                <h5>Q2: What is the difference between arrow function and regular function?</h5>
+                <p>Ans: Arrow functions are more concise and lexically bind the value of this.
+                        They lack their own arguments and can't be used as constructors, while regular functions have flexible this binding and support arguments.</p>
+            </li>
+            <li>
+                <h5>Q3: What is the difference between map, forEach, filter, find?</h5>
+                <p>
+                    Ans: map returns a new array by applying a provided function to each element and creating a new element with the returned value.
+                        forEach executes a provided function once for each array element, without creating a new array. Used for side effects.
+                        filter creates a new array with all elements that pass a test implemented by the provided function.
+                        find returns the first element in the array that satisfies a provided testing function, or undefined if none is found.
+                        All these methods operate on arrays in JavaScript, but they have different purposes and outcomes.
+                </p>
+            </li>
+            <li>
+                <h5>Q4: Why we use template string?</h5>
+                <p>Ans: Template strings in JavaScript enable interpolation of variables and expressions within strings, facilitate multiline text without escape characters, 
+                        and improve code readability for dynamic content creation, making complex string manipulation more concise and developer-friendly.</p>
+             </li>
+        </ul>
+    `
+}
 
 // loader unit
 const toggleLoader = (spinner) => {
